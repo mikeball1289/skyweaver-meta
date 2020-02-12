c7bffb4400786a99abd09a3bf8dc9151fefa7dd4
Mike Ball <mike.ball@camis.com>
2020-02-12 08:34:00 -0500

    Update spacing

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,26 +6,26 @@ import PrismsPage from '../../pages/Prisms/PrismsPage'
 import './App.css';
 
 const App = () => {
-  return (
-    <div>
-      <Router>
-        <Link to='/decks'>Decks</Link>
-        <span className='App-divider'>|</span>
-        <Link to='/cards'>Cards</Link>
-        <span className='App-divider'>|</span>
-        <Link to='/prisms'>Prisms</Link>
+    return (
+        <div>
+            <Router>
+                <Link to='/decks'>Decks</Link>
+                <span className='App-divider'>|</span>
+                <Link to='/cards'>Cards</Link>
+                <span className='App-divider'>|</span>
+                <Link to='/prisms'>Prisms</Link>
 
-        <Switch>
-          <Route default path='/decks' component={ DecksPage }/>
-          <Route path='/cards' component={ CardsPage }/>
-          <Route path='/prisms' component={ PrismsPage }/>
-          <Route path='/' render={() =>
-            <Redirect to='/decks' />
-          } />
-        </Switch>
-      </Router>
-    </div>
-  );
+                <Switch>
+                    <Route default path='/decks' component={ DecksPage }/>
+                    <Route path='/cards' component={ CardsPage }/>
+                    <Route path='/prisms' component={ PrismsPage }/>
+                    <Route path='/' render={() =>
+                        <Redirect to='/decks' />
+                    } />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
